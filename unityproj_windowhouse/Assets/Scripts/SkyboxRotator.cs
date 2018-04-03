b47814d4fb7c165ce811442146657b4fd4099cac
natalielh <nlehuenen@gmail.com>
2018-04-03 02:39:36 -0400

    update scene, add torch

---
+++ unityproj_windowhouse/Assets/Scripts/SkyboxRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour {
+
+	[Range(-20.0f,20.0f)]
+	public float speed;
+
+	//current rotation
+	private float curRot = 0;
+
+	void Update () {
+		curRot += speed * Time.deltaTime;
+		curRot %= 360;
+		RenderSettings.skybox.SetFloat("_Rotation", curRot);
+	}
+
+
+}
