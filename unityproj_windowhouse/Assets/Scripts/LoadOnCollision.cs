9d7511f5b15250e2f88c4f39ae456c32a30e006b
natalielh <nlehuenen@gmail.com>
2019-03-28 23:10:14 -0400

    Fixed scene loading error
    Everything is working right now on Windows 10.

--- unityproj_windowhouse/Assets/Scripts/LoadOnCollision.cs
+++ unityproj_windowhouse/Assets/Scripts/LoadOnCollision.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadOnCollision : MonoBehaviour {
+public class LoadOnCollision : MonoBehaviour
+{
 
-	/*
+    /*
 		This script loads a specified scene by its index number upon collision
 		with the Player's collider
 		
@@ -13,15 +14,17 @@ public class LoadOnCollision : MonoBehaviour {
 		set 'Is Trigger' to TRUE
 	*/
 
-	// the index number of the scene to be loaded
-	public int next_scene_index;
+    // the index number of the scene to be loaded
+    public int next_scene_index;
 
-	void OnTriggerEnter(Collider collider)
-	{
-		// if the collider we are colliding with is the Player's collider
-		if(collider.gameObject.tag.Equals("Player") == true){
-			// load the scene specified by the inspector-settable variable
-			SceneManager.LoadScene(next_scene_index);
-		}
-	}
+    void OnTriggerEnter(Collider collider)
+    {
+        // if the collider we are colliding with is the Player's collider
+        if (collider.gameObject.tag.Equals("Player") == true)
+        {
+            // load the scene specified by the inspector-settable variable
+            SceneManager.LoadScene(next_scene_index);
+            Debug.Log("SCENE LOADED! INDEX = " + next_scene_index);
+        }
+    }
 }
